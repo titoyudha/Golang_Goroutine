c1e3b30ccf43fe78ef108dc4f9a51240da5eea9b
Tito Yudha <tito.yudha.bimashika@gmail.com>
2021-09-12 23:51:19 +0800

    Race Condition

--- channel_test.go
+++ channel_test.go
@@ -2,6 +2,7 @@ package goroutine
 
 import (
 	"fmt"
+	"strconv"
 	"testing"
 	"time"
 )
@@ -25,7 +26,7 @@ func TestChannel(t *testing.T) {
 	<--------------------------------Channel Sebagai Parameter---------------------------------------->
 */
 
-func ChannelParam(channel chan string) {
+func GiveMeResponse(channel chan string) {
 	time.Sleep(2 * time.Second)
 	channel <- "Hello World2"
 }
@@ -34,7 +35,7 @@ func TestChannelasParam(t *testing.T) {
 	channel := make(chan string)
 	defer close(channel)
 
-	go ChannelParam(channel)
+	go GiveMeResponse(channel)
 	data := <-channel
 	fmt.Println(data)
 
@@ -88,4 +89,74 @@ func TestBuffered(t *testing.T) {
 
 	time.Sleep(5 * time.Second)
 	fmt.Println("Done Sending data")
+}
+
+/*
+	<--------------------------------Range Channel---------------------------------------->
+*/
+
+func TestRangeChannel(t *testing.T) {
+	channel := make(chan string)
+
+	go func() {
+		for i := 0; i < 10; i++ {
+			channel <- "Perulangan ke " + strconv.Itoa(i)
+		}
+		defer close(channel)
+	}()
+
+	for data := range channel {
+		fmt.Println("Received data ", data)
+	}
+	fmt.Println("done sending data")
+}
+
+/*
+	<--------------------------------Select Channel---------------------------------------->
+*/
+
+func TestSelectChannel(t *testing.T) {
+	channel1 := make(chan string)
+	channel2 := make(chan string)
+	defer close(channel1)
+	defer close(channel2)
+
+	go GiveMeResponse(channel1)
+	go GiveMeResponse(channel2)
+
+	counter := 0
+
+	for {
+		select {
+		case data := <-channel1:
+			fmt.Println("Data From Channel 1", data)
+			counter++
+		case data := <-channel2:
+			fmt.Println("Received From Channel 2", data)
+			counter++
+		default:
+			fmt.Println("Waiting...")
+		}
+		if counter == 2 {
+			break
+		}
+	}
+}
+
+/*
+	<--------------------------------Race Condition---------------------------------------->
+*/
+
+func TestRaceCondition(t *testing.T) {
+	x := 0
+
+	for i := 1; i <= 1000; i++ {
+		go func() {
+			for j := 1; j <= 100; j++ {
+				x = x + 1
+			}
+		}()
+	}
+	time.Sleep(5 * time.Second)
+	fmt.Println("Counter ", x)
 }
