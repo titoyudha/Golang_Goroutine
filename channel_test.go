16ae7d63bc474ee400353de110773f36f3761af5
Tito Yudha <tito.yudha.bimashika@gmail.com>
2021-09-16 01:16:49 +0800

    fixing deadlock

--- channel_test.go
+++ channel_test.go
@@ -246,19 +246,36 @@ func Transfer(user1 *UserBalance, user2 *UserBalance, amount int) {
 	user1.Lock()
 	fmt.Println("Lock", user1.Name)
 	user1.Change(-amount)
+	user1.Unlock()
+	//Kita Harus mengunlock pada setiap mutex untuk menghindari kondisi Deadlock
 
 	time.Sleep(2 * time.Second)
 
 	user2.Lock()
 	fmt.Println("Lock", user2.Name)
 	user2.Change(amount)
+	user2.Unlock()
+	//Kita Harus mengunlock pada setiap mutex untuk menghindari kondisi Deadlock
 
 	time.Sleep(2 * time.Second)
 
-	user1.Unlock()
-	user2.Unlock()
 }
 
 func TestDeadLock(t *testing.T) {
+	user1 := UserBalance{
+		Name:    "Mock1",
+		Balance: 1000000,
+	}
+	user2 := UserBalance{
+		Name:    "Mock2",
+		Balance: 1000000,
+	}
 
+	go Transfer(&user1, &user2, 600000)
+	go Transfer(&user2, &user1, 200000)
+
+	time.Sleep(5 * time.Second)
+
+	fmt.Println("User ", user1.Name, "Balance ", user1.Balance)
+	fmt.Println("User ", user2.Name, "Balance ", user2.Balance)
 }
