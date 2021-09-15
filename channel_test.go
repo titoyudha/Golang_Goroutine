2bb9e576a1b7e91b3290a8bcd95a87743eb1b16b
Tito Yudha <tito.yudha.bimashika@gmail.com>
2021-09-15 23:31:32 +0800

    DeadLock

--- channel_test.go
+++ channel_test.go
@@ -3,6 +3,7 @@ package goroutine
 import (
 	"fmt"
 	"strconv"
+	"sync"
 	"testing"
 	"time"
 )
@@ -159,4 +160,105 @@ func TestRaceCondition(t *testing.T) {
 	}
 	time.Sleep(5 * time.Second)
 	fmt.Println("Counter ", x)
+}
+
+/*
+	<--------------------------------Sync.Mutex---------------------------------------->
+*/
+
+func TestSyncMutex(t *testing.T) {
+	x := 0
+	var mutex sync.Mutex
+
+	for i := 1; i <= 1000; i++ {
+		go func() {
+			for j := 1; j <= 100; j++ {
+				mutex.Lock()
+				x = x + 1
+				mutex.Unlock()
+			}
+		}()
+	}
+	time.Sleep(5 * time.Second)
+	fmt.Println("Counter ", x)
+}
+
+/*
+	<--------------------------------Read Write Mutex---------------------------------------->
+*/
+
+type BankAccount struct {
+	RwMutex sync.RWMutex
+	Balance int
+}
+
+func (account *BankAccount) AddBalance(amount int) {
+	account.RwMutex.Lock()
+	account.Balance = account.Balance + amount
+	account.RwMutex.Unlock()
+}
+
+func (account *BankAccount) GetBalance() int {
+	account.RwMutex.RLock() //Read Lock
+	balance := account.Balance
+	account.RwMutex.RUnlock() //Read Unlock
+	return balance
+}
+
+func TestRWMutex(t *testing.T) {
+	account := BankAccount{}
+
+	for i := 0; i < 100; i++ {
+		go func() {
+			for j := 0; j < 100; j++ {
+				account.AddBalance(1)
+				fmt.Println(account.GetBalance())
+			}
+		}()
+	}
+	time.Sleep(5 * time.Second)
+	fmt.Println("Total = ", account.GetBalance())
+}
+
+/*
+	<--------------------------------Dead Lock---------------------------------------->
+*/
+
+type UserBalance struct {
+	sync.Mutex
+	Name    string
+	Balance int
+}
+
+func (user *UserBalance) Lock() {
+	user.Mutex.Lock()
+}
+
+func (user *UserBalance) Unlock() {
+	user.Mutex.Unlock()
+}
+
+func (user *UserBalance) Change(amount int) {
+	user.Balance = user.Balance + amount
+}
+
+func Transfer(user1 *UserBalance, user2 *UserBalance, amount int) {
+	user1.Lock()
+	fmt.Println("Lock", user1.Name)
+	user1.Change(-amount)
+
+	time.Sleep(2 * time.Second)
+
+	user2.Lock()
+	fmt.Println("Lock", user2.Name)
+	user2.Change(amount)
+
+	time.Sleep(2 * time.Second)
+
+	user1.Unlock()
+	user2.Unlock()
+}
+
+func TestDeadLock(t *testing.T) {
+
 }
